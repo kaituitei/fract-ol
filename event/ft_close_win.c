70bb454feba234ec4f9da0534d7c8091bec76ec4
fuku <killvader8550@gmail.com>
2026-05-18 07:59:08 +0700

    refactor

--- event/ft_close_win.c
+++ event/ft_close_win.c
@@ -1,8 +1,8 @@
 #include "../fractol.h"
 
-int	ft_close_win(int keycode, t_data *img)
+int	ft_close_win(int keycode, t_data *data)
 {
-    (void *)keycode;
-    mlx_destroy_window(img->mlx, img->win);
+    (void)keycode;
+    mlx_destroy_window(data->mlx, data->mlx_win);
     return (0);
 }
